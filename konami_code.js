9ba76d0b70799f17afa652de5baaa067c6b42251
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-08 23:46:01 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -53,14 +53,14 @@ function init() {
 
   document.body.addEventListener("keydown", (e) => {
     const key = e.key
-    
+
     idx = (codes[idx] === key) ? ++idx : 0
-    
+
     if (idx === codes.length) {
       window.alert("Hurray!");
       idx = 0
     }
-    
+
   });
 }
 
