6531a2d5d3962a57a032eed337c97c40575a75ed
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-08 23:45:58 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,4 @@
-const codes = [
+/*const codes = [
   "ArrowUp",
   "ArrowUp",
   "ArrowDown",
@@ -32,5 +32,36 @@ console.log(key);
   }
 })
 };
+
+init();*/
+
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+
+function init() {
+  let idx = 0
+
+  document.body.addEventListener("keydown", (e) => {
+    const key = e.key
+    
+    idx = (codes[idx] === key) ? ++idx : 0
+    
+    if (idx === codes.length) {
+      window.alert("Hurray!");
+      idx = 0
+    }
+    
+  });
+}
 
 init();
